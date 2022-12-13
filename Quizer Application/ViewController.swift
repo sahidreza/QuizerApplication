276302da931d227b453270f81e2605e3e954cf75
sahidcse15@gmail.com <sahidcse15@gmail.com>
2022-12-13 16:47:32 +0530

    assign part completed

--- Quizer Application/ViewController.swift
+++ Quizer Application/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var trueButton: UIButton!
+    
+    
+    @IBOutlet weak var falseButton: UIButton!
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // UI PartCOmplete Now is Funtionla part
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        print(sender.titleLabel?.text)
+    }
+    
 }
 
